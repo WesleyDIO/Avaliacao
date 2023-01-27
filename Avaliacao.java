f3b2a9719e55d5e01d380cb2fd27425904818063
Wesley Diorrani Ferreira <wesley_d_ferreira@estudante.sesisenai.org.br>
2023-01-27 14:49:30 -0300

    Update Avaliacao.java

--- Avaliacao.java
+++ Avaliacao.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class sudoku {
 	public static void main(String[] args) {
 	    Scanner entrada = new Scanner(System.in);
 	    System.out.println("Digite quantos jogadores iram participar: ");
 	    int numJogadores = entrada.nextInt();
+	    
 	    String[] nomesJogadores = new String[numJogadores];
 	    int[][][] matrizesJogadores = new int[numJogadores][9][9];
 	    for (int i = 0; i < numJogadores; i++) {
@@ -12,6 +14,7 @@ public class sudoku {
 	        
 	        for (int j = 0; j < 9; j++) {
 	            for (int k = 0; k < 9; k++) {
+	            	tabela(matrizesJogadores);
 	            	System.out.println("Digite o valor da linha "+ (j+1)+ " Na coluna "+(k+1)+": ");
 	                matrizesJogadores[i][j][k] = entrada.nextInt();
 	                
@@ -21,15 +24,15 @@ public class sudoku {
 	    
 		    for (int i = 0; i < numJogadores; i++) {
 		    	System.out.println("\nTabela do Jogador: "+nomesJogadores[i]);
-		    	System.out.println("============================================");
+		    	System.out.println("================================================================");
 		        for (int j = 0; j < 9; j++) {
 		        	for(int k = 0; k < 9; k++) {
-		            System.out.print(matrizesJogadores[i][j][k] + "");
+		            System.out.print(" | "+matrizesJogadores[i][j][k] + " | ");
 		        }
-		       System.out.println("");
+		       System.out.println("*");
 		        }
 		    }
-		    System.out.println("================================================");
+		    System.out.println("====================================================================");
 		
 	    for (int i = 0; i < numJogadores; i++) {
 	        System.out.print("Sudoku " + (i+1) + ": " + nomesJogadores[i] + " ");
@@ -96,5 +99,20 @@ public class sudoku {
 	    }
 	    return true;
 	}
-	 
+	 public static void tabela (int[][][] matrizesJogadores) {
+		
+	        System.out.println(" | "+matrizesJogadores[0][0][0]+" "+matrizesJogadores[0][0][1]+" "+matrizesJogadores[0][0][2]+" | "+matrizesJogadores[0][0][3]+" "+matrizesJogadores[0][0][4]+" "+matrizesJogadores[0][0][5]+" | "+matrizesJogadores[0][0][6]+" "+matrizesJogadores[0][0][7]+" "+matrizesJogadores[0][0][8]+" | ");
+	        System.out.println(" | "+matrizesJogadores[0][1][0]+" "+matrizesJogadores[0][1][1]+" "+matrizesJogadores[0][1][2]+" | "+matrizesJogadores[0][1][3]+" "+matrizesJogadores[0][1][4]+" "+matrizesJogadores[0][1][5]+" | "+matrizesJogadores[0][1][6]+" "+matrizesJogadores[0][1][7]+" "+matrizesJogadores[0][1][8]+" | ");
+	        System.out.println(" | "+matrizesJogadores[0][2][0]+" "+matrizesJogadores[0][2][1]+" "+matrizesJogadores[0][2][2]+" | "+matrizesJogadores[0][2][3]+" "+matrizesJogadores[0][2][4]+" "+matrizesJogadores[0][2][5]+" | "+matrizesJogadores[0][2][6]+" "+matrizesJogadores[0][2][7]+" "+matrizesJogadores[0][2][8]+" | ");
+	        System.out.println("  -----------------------");
+	        System.out.println(" | "+matrizesJogadores[0][3][0]+" "+matrizesJogadores[0][3][1]+" "+matrizesJogadores[0][3][2]+" | "+matrizesJogadores[0][3][3]+" "+matrizesJogadores[0][3][4]+" "+matrizesJogadores[0][3][5]+" | "+matrizesJogadores[0][3][6]+" "+matrizesJogadores[0][3][7]+" "+matrizesJogadores[0][3][8]+" | ");
+	        System.out.println(" | "+matrizesJogadores[0][4][0]+" "+matrizesJogadores[0][4][1]+" "+matrizesJogadores[0][4][2]+" | "+matrizesJogadores[0][4][3]+" "+matrizesJogadores[0][4][4]+" "+matrizesJogadores[0][4][5]+" | "+matrizesJogadores[0][4][6]+" "+matrizesJogadores[0][4][7]+" "+matrizesJogadores[0][4][8]+" | ");
+	        System.out.println(" | "+matrizesJogadores[0][5][0]+" "+matrizesJogadores[0][5][1]+" "+matrizesJogadores[0][5][2]+" | "+matrizesJogadores[0][5][3]+" "+matrizesJogadores[0][5][4]+" "+matrizesJogadores[0][5][5]+" | "+matrizesJogadores[0][5][6]+" "+matrizesJogadores[0][5][7]+" "+matrizesJogadores[0][5][8]+" | ");
+	        System.out.println("  -----------------------");
+	        System.out.println(" | "+matrizesJogadores[0][6][0]+" "+matrizesJogadores[0][6][1]+" "+matrizesJogadores[0][6][2]+" | "+matrizesJogadores[0][6][3]+" "+matrizesJogadores[0][6][4]+" "+matrizesJogadores[0][6][5]+" | "+matrizesJogadores[0][6][6]+" "+matrizesJogadores[0][6][7]+" "+matrizesJogadores[0][6][8]+" | ");
+	        System.out.println(" | "+matrizesJogadores[0][7][0]+" "+matrizesJogadores[0][7][1]+" "+matrizesJogadores[0][7][2]+" | "+matrizesJogadores[0][7][3]+" "+matrizesJogadores[0][7][4]+" "+matrizesJogadores[0][7][5]+" | "+matrizesJogadores[0][7][6]+" "+matrizesJogadores[0][7][7]+" "+matrizesJogadores[0][7][8]+" | ");
+	        System.out.println(" | "+matrizesJogadores[0][8][0]+" "+matrizesJogadores[0][8][1]+" "+matrizesJogadores[0][8][2]+" | "+matrizesJogadores[0][8][3]+" "+matrizesJogadores[0][8][4]+" "+matrizesJogadores[0][8][5]+" | "+matrizesJogadores[0][8][6]+" "+matrizesJogadores[0][8][7]+" "+matrizesJogadores[0][8][8]+" | ");
+	        
+	    }
      }
+     
